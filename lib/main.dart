79a7e0b890ea219f64869cd841f34abf6f5aad25
sbayat023 <sbayat023@student.wethinkcode.co.za>
2024-11-13 12:14:26 +0200

    Updated project to render shaders

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,50 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-void main() {
+late FragmentProgram fragmentProgram;
+
+Future<void> main() async{
+  fragmentProgram = await FragmentProgram.fromAsset('shaders/FractalWorldStar.frag');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: CustomPaint(
+        painter: MyPainter(
+            Colors.green,
+            shader: fragmentProgram.fragmentShader(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
+}
+
+class MyPainter extends CustomPainter{
+  MyPainter(this.color, {required this.shader});
+
+  final Color color;
+  final FragmentShader shader;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //uSize vec2 (0 to 1)
+    shader.setFloat(0, size.width);
+    shader.setFloat(1, size.height);
+
+    //uColor vec4 (2 - 5)
+    shader.setFloat(2, color.red.toDouble() / 255);
+    shader.setFloat(3, color.green.toDouble() / 255);
+    shader.setFloat(4, color.blue.toDouble() / 255);
+    shader.setFloat(5, color.alpha.toDouble() / 255);
+
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), Paint()..shader = shader);
+  }
+
+  @override
+  bool shouldRepaint(MyPainter oldDelegate) => color != oldDelegate.color;
 }
---
+++ shaders/FractalWorldStar.frag
@@ -0,0 +1,86 @@
+/*#version 460 core
+precision mediump float;
+
+uniform float u_time;
+uniform vec2 u_resolution;
+
+float sdfSphere(vec2 uv, float size, float intencity){
+    return pow(length(uv) * size,intencity);
+}
+
+float sdfStar(vec2 uv){
+    float d = pow(length(uv.x * uv.y),0.4);
+    return pow(d + sdfSphere(uv,1.,50.),.5);
+}
+
+float sdfStarSingle(vec2 uv){
+    float d = pow(length(uv.x * uv.y),0.4);
+    return pow(d + sdfSphere(uv,1.,20.),.5);
+}
+
+vec3 palette(float t){
+    vec3 a = vec3(.5,.5,.5);
+    vec3 b = vec3(.5,.5,.5);
+    vec3 c = vec3(1.,1.,1.);
+    vec3 d = vec3(0.5843, 0.2235, 0.9216);
+    return a - b * cos(6.28318 - (c * t + d));
+}
+
+void main() {
+
+    vec2 uv = (gl_FragCoord.xy * 2. - u_resolution) / u_resolution.y;
+    vec2 uvOriginal = uv;
+    vec3 final_colour,col;
+    float d,angle = u_time * 2.;
+
+    //uv *= mat2(cos(.8),-sin(.8),sin(.8),cos(.8));
+
+
+    for(int i = 0; i < 4; i++){
+
+        //Wave
+        uv = fract(uv * 1.75) - 0.5;
+        float d = sdfStar(uv);
+        vec3 col = palette(d + (angle));
+    
+        d = sin(d*4. + u_time) / 8.;
+        d = 0.02/d;
+        d = pow(d,2.);
+        d = clamp(d,.2,.5);
+
+        //Star bg
+        float dbg = sdfStarSingle(uvOriginal);
+    
+        dbg = sin(dbg*.5 - u_time) / (clamp(sin(u_time) * 10.,2.,10.));
+        dbg = 0.02/dbg;
+        dbg *= 2.;
+        dbg = pow(dbg,2.5);
+
+        float final_alpha = (d * dbg * 2.);
+
+        final_colour += (col * final_alpha);
+    }
+
+    gl_FragColor = vec4(final_colour,1.0);
+    
+}
+*/
+#version 460 core
+#include <flutter/runtime_effect.glsl>
+
+uniform vec2 uSize;
+uniform vec4 uColor;
+
+out vec4 FragColor;
+
+float sdfSphere(vec2 uv, float size){
+    return length(uv) * size;
+}
+
+void main() {
+    vec2 uv = ((FlutterFragCoord() / uSize) * 2.) - 1.;
+
+    vec4 colorAlphaAdded = vec4(uColor.rgb * uColor.a, uColor.a);
+
+    FragColor = colorAlphaAdded * (0.2/sdfSphere(uv,1.));
+}
